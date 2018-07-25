f3497285c1d1d409588f44f6163e40d878bac480
Allie321 <allisonroberson04@gmail.com>
2018-07-25 21:00:30 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,11 @@
-# write your method here
+# crazy_strings
 
-def crazy_strings(a,b)
-
+def crazy_strings
+"hello friends".upcase.reverse
 end
+puts crazy_strings
+
+def crazy_strings
+  "hello friends".gsub("s","z")
+end
+puts crazy_strings
